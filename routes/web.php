9ee6146b08a2cab110e3375dac096e0d4b8c00ac
Fernando <fernandogarciadelrio76@gmail.com>
2026-05-24 22:28:40 -0300

    Agrega ruta temporal migracion checkout url

--- routes/web.php
+++ routes/web.php
@@ -448,4 +448,10 @@ Route::get('/generar-turnos-semana', function () {
     return 'Turnos generados correctamente para los próximos 7 días hábiles';
 });
 
+Route::get('/migrar-checkout-url', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+
+    return 'Migración checkout_url ejecutada';
+});
+
 require __DIR__ . '/settings.php';
