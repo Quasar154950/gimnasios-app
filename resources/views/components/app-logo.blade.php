224a46b9f9f0ed2dfb20529cdb5913c7c7b78949
Fernando <fernandogarciadelrio76@gmail.com>
2026-05-26 12:05:48 -0300

    Mejora branding y logo sidebar SportGym

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -5,23 +5,39 @@
 @php
     $user = auth()->user();
 
-    $nombreEstudio = $user->nombre_estudio ?? 'Mi Estudio Jurídico';
+    $nombreEstudio = $user->nombre_estudio ?? 'SportGym Tandil';
 
-    $logo = $user->logo_estudio
-        ? asset($user->logo_estudio)
-        : asset('images/logo.png');
+    $logo = asset('images/logo-sportgym.png');
 @endphp
 
 @if($sidebar)
+
     <flux:sidebar.brand name="{{ $nombreEstudio }}" {{ $attributes }}>
+
         <x-slot name="logo" class="flex items-center justify-center">
-            <img src="{{ $logo }}" class="h-10 w-10 object-cover rounded-full" />
+
+            <img
+                src="{{ $logo }}"
+                class="h-12 w-12 object-contain"
+            />
+
         </x-slot>
+
     </flux:sidebar.brand>
+
 @else
+
     <flux:brand name="{{ $nombreEstudio }}" {{ $attributes }}>
+
         <x-slot name="logo" class="flex items-center justify-center">
-            <img src="{{ $logo }}" class="h-8 w-8 object-cover rounded-full" />
+
+            <img
+                src="{{ $logo }}"
+                class="h-10 w-10 object-contain"
+            />
+
         </x-slot>
+
     </flux:brand>
+
 @endif
