815eedfaca3304bb4d4c367b3d4522b22b0c754a
Fernando <fernandogarciadelrio76@gmail.com>
2026-05-28 08:47:58 -0300

    Mejora responsive y fondo oscuro app socio

--- resources/views/cliente/dashboard.blade.php
+++ resources/views/cliente/dashboard.blade.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-zinc-100">
+<body class="bg-zinc-950">
 
     @php
         $cliente = \App\Models\Cliente::where('user_id', auth()->id())->first();
