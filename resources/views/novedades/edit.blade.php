41a0772fe83d6726a4764fc8bda599a440b3d07a
Fernando <fernandogarciadelrio76@gmail.com>
2026-07-24 12:55:07 -0300

    Mejorar visualizacion de imagenes en edicion

--- resources/views/novedades/edit.blade.php
+++ resources/views/novedades/edit.blade.php
@@ -232,7 +232,7 @@
                         id="vistaPrevia"
                         src=""
                         alt="Vista previa de la imagen"
-                        class="max-h-96 w-full rounded-2xl border border-zinc-700 object-cover"
+                        class="max-h-[32rem] w-full rounded-2xl border border-zinc-700 bg-zinc-950 object-contain"
                     >
                 </div>
 
